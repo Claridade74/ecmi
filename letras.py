467983e1541e0f0a02d0fff96903550e7db8b63a
Clarissa Treptow <168579472+Claridade74@users.noreply.github.com>
2024-06-14 10:29:49 -0300

    Update letras.py

--- letras.py
+++ letras.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import string
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
